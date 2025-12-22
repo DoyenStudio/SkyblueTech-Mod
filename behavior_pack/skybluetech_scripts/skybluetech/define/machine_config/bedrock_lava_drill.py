9e80e4a19879e70dbee59a5f7d7f1cbeff9c4781
SuperScript <2528622340@qq.com>
2025-12-22 22:50:11 +0800

    todo: 暂时提交工作区临时文件

---
+++ behavior_pack/skybluetech_scripts/skybluetech/define/machine_config/bedrock_lava_drill.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+IO_ENERGY = "skybluetech:drill_io_energy"
+IO_FLUID2 = "skybluetech:drill_io_fluid"
+IO_ITEM = "skybluetech:fermenter_io_item"
+HEAVY_FRAME = "skybluetech:heavy_frame"
+STRUCTURE_PATTERN_MAPPING = {
+    "H": HEAVY_FRAME,
+}
+STRUCTURE_PATTERN ={
+    0: [
+        "   H   ",
+        "       ",
+        "       ",
+        "H  #  H",
+        "       ",
+        "       ",
+        "   H   ",
+    ],
+    1: [
+        "       ",
+        "   H   ",
+        "       ",
+        " H   H ",
+        "       ",
+        "   H   ",
+        "       ",
+    ],
+    2: [
+        "       ",
+        "       ",
+        "   H   ",
+        "  H H  ",
+        "   H   ",
+        "       ",
+        "       ",
+    ]
+}

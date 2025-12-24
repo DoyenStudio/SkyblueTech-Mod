01ccd49fe8813b199dece573085c4d3cdaef6ec3
SuperScript <2528622340@qq.com>
2025-12-24 12:59:10 +0800

    m: update docs

--- behavior_pack/skybluetech_scripts/modMain.py
+++ behavior_pack/skybluetech_scripts/modMain.py
@@ -5,7 +5,10 @@ SkyblueTech Mod | 蔚蓝科技
 交流群:   532685971
 作者:     SuperScript
 联动请找: SuperScript
+开源地址(别解包了! 来仓库看完整的含类型注释的源代码!): https://github.com/SuperScript-PRC/SkyblueTech-Mod
 问题反馈: https://github.com/SuperScript-PRC/SkyblueTech-Mod/issues
+
+开放式 API: skybluetech_scripts.skybluetech.export
 """
 
 from .tooldelta.mod_main import ToolDeltaMod, RegisterMod

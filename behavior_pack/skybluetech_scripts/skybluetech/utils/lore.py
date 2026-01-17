9379a07476a21bb1edc6ec55fe25fd89a30b2fd1
SuperScript <2528622340@qq.com>
2026-01-17 17:04:47 +0800

    feat: lore: SetLoreAuto

--- behavior_pack/skybluetech_scripts/skybluetech/utils/lore.py
+++ behavior_pack/skybluetech_scripts/skybluetech/utils/lore.py
@@ -1,9 +1,25 @@
 # coding=utf-8
+from skybluetech_scripts.tooldelta.utils import nbt
 
-def SetLoreAtPos(userdata, pos, lore):
-    # type: (dict, int, str) -> None
+def SetLoreAtPos(userdata, pos, lore, lore_name=None):
+    # type: (dict, int, str, str | None) -> None
     userdata["display"]["Lore"][pos]["__value__"] = lore
+    if lore_name is not None:
+        userdata.setdefault("lore_pos", {})[lore_name] = nbt.Int(pos)
 
-def GetLorePos(userdata, name):
-    # type: (dict, str) -> int
-    return userdata.get("lore_pos", {}).get(name, {}).get("__value__", 0)
+def GetLorePos(userdata, name, default=0):
+    # type: (dict, str, int) -> int
+    return nbt.GetValueWithDefault(userdata.get("lore_pos", {}), name, default)
+
+def SetLoreAuto(userdata, lore_name, lore):
+    # type: (dict, str, str) -> None
+    lore_pos = GetLorePos(userdata, lore_name, -1)
+    lores = userdata.setdefault("display", {}).setdefault("Lore", [])
+    if lore_pos == -1:
+        lore_pos = len(lores) - 1
+        lores.append(nbt.String(lore))
+        userdata.setdefault("lore_pos", {})[lore_name] = nbt.Int(lore_pos)
+    else:
+        lores[lore_pos] = nbt.String(lore)
+
+    

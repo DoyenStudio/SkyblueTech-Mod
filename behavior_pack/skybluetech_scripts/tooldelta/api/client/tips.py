f28f2e6a433a3ec71ce3d70f19b5c97e697fb31e
SuperScript <2528622340@qq.com>
2026-02-13 20:16:17 +0800

    fix: 修复函数拼写错误

--- behavior_pack/skybluetech_scripts/tooldelta/api/client/tips.py
+++ behavior_pack/skybluetech_scripts/tooldelta/api/client/tips.py
@@ -1,13 +1,14 @@
 from ...internal import ClientComp, ClientLevelId
 from ..internal.cacher import MethodCacher
 
-_setOnePopupNotice = MethodCacher(lambda:ClientComp.CreateGame(ClientLevelId).SetPopupNotice)
+_setOnePopupNotice = MethodCacher(
+    lambda: ClientComp.CreateGame(ClientLevelId).SetPopupNotice
+)
 
-def SePopupNotice(message, subtitle="§6提示§f"):
+
+def SetPopupNotice(message, subtitle="§6提示§f"):
     # type: (str, str) -> None
     _setOnePopupNotice(message, subtitle)
 
 
-__all__ = [
-    "SePopupNotice"
-]
+__all__ = ["SetPopupNotice"]

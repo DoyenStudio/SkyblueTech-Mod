64a8174a9bce9de2012279664ec1e447be9700e6
SuperScript <2528622340@qq.com>
2026-02-13 20:13:30 +0800

    feat: tooldelta: entity 加入 GetFootPos; 格式化代码

--- behavior_pack/skybluetech_scripts/tooldelta/api/client/entity.py
+++ behavior_pack/skybluetech_scripts/tooldelta/api/client/entity.py
@@ -3,17 +3,31 @@ from ...define.item import Item
 from ...internal import ClientComp, ClientLevelId, GetClient
 from ..internal.cacher import MethodCacher
 
-_addDropItemToWorld = MethodCacher(lambda :ClientComp.CreateItem(ClientLevelId).AddDropItemToWorld)
+_addDropItemToWorld = MethodCacher(
+    lambda: ClientComp.CreateItem(ClientLevelId).AddDropItemToWorld
+)
+
 
 def CreateDropItemModelEntity(dim, xyz, item, bob_speed=0, spin_speed=0):
     # type: (int, tuple[float, float, float], Item, float, float) -> str
     return _addDropItemToWorld(item.marshal(), dim, xyz, bob_speed, spin_speed)
 
+
 def EvalMolangExpression(entity_id, expression):
     # type: (str, str) -> dict
     return ClientComp.CreateQueryVariable(entity_id).EvalMolangExpression(expression)
 
-SetDropItemTransform = MethodCacher(lambda :ClientComp.CreateItem(ClientLevelId).SetDropItemTransform)
-DeleteClientDropItemEntity = MethodCacher(lambda :ClientComp.CreateItem(ClientLevelId).DeleteClientDropItemEntity)
-CreateClientEntity = MethodCacher(lambda :GetClient().CreateClientEntityByTypeStr)
-DestroyClientEntity = MethodCacher(lambda :GetClient().DestroyClientEntity)
+
+def GetFootPos(entity_id):
+    # type: (str) -> tuple[float, float, float]
+    return ClientComp.CreatePos(entity_id).GetFootPos()
+
+
+SetDropItemTransform = MethodCacher(
+    lambda: ClientComp.CreateItem(ClientLevelId).SetDropItemTransform
+)
+DeleteClientDropItemEntity = MethodCacher(
+    lambda: ClientComp.CreateItem(ClientLevelId).DeleteClientDropItemEntity
+)
+CreateClientEntity = MethodCacher(lambda: GetClient().CreateClientEntityByTypeStr)
+DestroyClientEntity = MethodCacher(lambda: GetClient().DestroyClientEntity)

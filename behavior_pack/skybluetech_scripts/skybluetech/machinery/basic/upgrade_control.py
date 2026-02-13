ce2e4c99e36790f365a0597bdeda7db410e580da
SuperScript <2528622340@qq.com>
2026-02-13 20:11:52 +0800

    fix: 机器在世界加载完成时不使用其中的升级模块

--- behavior_pack/skybluetech_scripts/skybluetech/machinery/basic/upgrade_control.py
+++ behavior_pack/skybluetech_scripts/skybluetech/machinery/basic/upgrade_control.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 #
 from skybluetech_scripts.tooldelta.define.item import Item
+from ...define import flags
 from ...machinery_def.upgraders import *
+from ...transmitters.wire.logic import RequireEnergyFromNetwork
 from .base_machine import BaseMachine
 from .item_container import ItemContainer
 from .sp_control import SPControl
@@ -10,7 +12,7 @@ from .sp_control import SPControl
 class UpgradeControl(ItemContainer, SPControl):
     """
     代表可接受升级卡的机器基类。
-    
+
     派生自:
         `ItemContainer`
         `SPControl`
@@ -21,7 +23,7 @@ class UpgradeControl(ItemContainer, SPControl):
         allow_upgrader_tags (set[str]): 可接受的机器升级卡标签。
 
     需要调用 `__init__ (super)`
-    
+
     覆写:
         `IsValidInput`
         `OnSlotUpdate`
@@ -30,20 +32,24 @@ class UpgradeControl(ItemContainer, SPControl):
         `AddPower (super)`
         `SetDeactiveFlag (super)`
     """
-    upgrade_slot_start = 2 # type: int
-    upgrade_slots = 4 # type: int
-    allow_upgrader_tags = set() # type: set[str]
+
+    upgrade_slot_start = 2  # type: int
+    upgrade_slots = 4  # type: int
+    allow_upgrader_tags = set()  # type: set[str]
 
     def __init__(self, dim, x, y, z, block_entity_data):
         ItemContainer.__init__(self, dim, x, y, z, block_entity_data)
         SPControl.__init__(self, dim, x, y, z, block_entity_data)
         self._basic_max_rf_store = self.store_rf_max
-        self._origin_power = self.running_power
+        self._power_cost_relative = 1.0
         self.UpdateUpgraders(self.GetAllUpgraders())
 
     def InUpgradeSlot(self, slot):
         # type: (int) -> bool
-        return slot >= self.upgrade_slot_start and slot < self.upgrade_slot_start + self.upgrade_slots
+        return (
+            slot >= self.upgrade_slot_start
+            and slot < self.upgrade_slot_start + self.upgrade_slots
+        )
 
     def IsValidInput(self, slot, item):
         # type: (int, Item) -> bool
@@ -55,17 +61,49 @@ class UpgradeControl(ItemContainer, SPControl):
             and not self.otherSlotHasSameUpgrader(slot, item.id)
         )
 
+    def ReducePower(self, rf=None, bypass_upgraders=False):
+        # type: (int | None, bool) -> None
+        "PowerControl 方法, 由 UpgradeControl 覆写"
+        if rf is None:
+            rf = self.running_power
+        if not bypass_upgraders:
+            rf = round(rf * self._power_cost_relative)
+        BaseMachine.ReducePower(self, rf)
+
+    def PowerEnough(self, auto_require=True):
+        # type: (bool) -> bool
+        """
+        PowerControl 方法, 由 UpgradeControl 覆写
+
+        如果能量不足时先尝试向电网索取能源, 后自动将 flag 设置为缺少能源
+        """
+        res = self.store_rf >= round(self.running_power * self._power_cost_relative)
+        if res:
+            if self.HasDeactiveFlag(flags.DEACTIVE_FLAG_POWER_LACK):
+                self.UnsetDeactiveFlag(flags.DEACTIVE_FLAG_POWER_LACK)
+        elif auto_require:
+            RequireEnergyFromNetwork(self)
+            return self.PowerEnough(auto_require=False)
+        else:
+            self.SetDeactiveFlag(flags.DEACTIVE_FLAG_POWER_LACK)
+        return res
+
     def OnSlotUpdate(self, slot):
         # type: (int) -> None
         "超类方法更新升级槽数据。"
-        if slot < self.upgrade_slot_start or slot >= self.upgrade_slot_start + self.upgrade_slots:
+        if (
+            slot < self.upgrade_slot_start
+            or slot >= self.upgrade_slot_start + self.upgrade_slots
+        ):
             return
         self.UpdateUpgraders(self.GetAllUpgraders())
 
     def GetAllUpgraders(self):
         # type: () -> dict[str, int]
-        res = {} # type: dict[str, int]
-        for i in range(self.upgrade_slot_start, self.upgrade_slot_start + self.upgrade_slots):
+        res = {}  # type: dict[str, int]
+        for i in range(
+            self.upgrade_slot_start, self.upgrade_slot_start + self.upgrade_slots
+        ):
             item = self.GetSlotItem(i)
             if item is None:
                 continue
@@ -88,7 +126,7 @@ class UpgradeControl(ItemContainer, SPControl):
             # power
             power_neg += POWER_UPGRADER_MAPPINGS.get(upgrader, 0) * count
         self.SetSpeedRelative(speed_pos / speed_neg)
-        self.SetPower(int(self._origin_power * power_pos / power_neg))
+        self._power_cost_relative = power_pos / power_neg
 
     def HasUpgrader(self, item_id):
         # type: (str) -> bool
@@ -96,10 +134,17 @@ class UpgradeControl(ItemContainer, SPControl):
 
     def otherSlotHasSameUpgrader(self, slot, item_name):
         # type: (int, str) -> bool
-        slot_range = range(self.upgrade_slot_start, self.upgrade_slot_start + self.upgrade_slots)
+        slot_range = range(
+            self.upgrade_slot_start, self.upgrade_slot_start + self.upgrade_slots
+        )
         for i in slot_range:
             slotitem = self.GetSlotItem(i)
-            if slotitem is not None and i != slot and slotitem.id == item_name and i != slot:
+            if (
+                slotitem is not None
+                and i != slot
+                and slotitem.id == item_name
+                and i != slot
+            ):
                 return True
         return False
 

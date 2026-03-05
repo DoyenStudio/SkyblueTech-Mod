137812b29bd4425d5fb78273492c7d3f17a73f43
SuperScript <2528622340@qq.com>
2026-03-05 16:43:31 +0800

    style: 格式化代码

--- behavior_pack/skybluetech_scripts/skybluetech/common/define/facing.py
+++ behavior_pack/skybluetech_scripts/skybluetech/common/define/facing.py
@@ -9,9 +9,11 @@ NEIGHBOR_BLOCKS_ENUM = (
 
 OPPOSITE_FACING = (1, 0, 3, 2, 5, 4)
 
+
 def GetFacingByDxyz(dx, dy, dz):
     # type: (int, int, int) -> int
     return NEIGHBOR_BLOCKS_ENUM.index((dx, dy, dz))
+
 
 FACING_ZHCN = {0: "下", 1: "上", 2: "北", 3: "南", 4: "西", 5: "东"}
 FACING_EN = {0: "down", 1: "up", 2: "north", 3: "south", 4: "west", 5: "east"}
@@ -22,14 +24,7 @@ FACING_DXZ = (
     (-1, 0),
     (1, 0),
 )
-FACING_DXYZ = (
-    (0, -1, 0),
-    (0, 1, 0),
-    (0, 0, -1),
-    (0, 0, 1),
-    (-1, 0, 0),
-    (1, 0, 0),
-)
+FACING_DXYZ = ((0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1), (-1, 0, 0), (1, 0, 0))
 DXYZ_FACING = {
     (0, 0, 1): 3,
     (0, 0, -1): 2,

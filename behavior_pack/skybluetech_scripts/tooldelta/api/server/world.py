7c875170e4084675315461f5ff0d88d8ecf3eeef
SuperScript <2528622340@qq.com>
2026-03-12 08:58:42 +0800

    minor feat

--- behavior_pack/skybluetech_scripts/tooldelta/api/server/world.py
+++ behavior_pack/skybluetech_scripts/tooldelta/api/server/world.py
@@ -4,21 +4,30 @@ from ...internal import ServerComp, ServerLevelId
 from ..internal.cacher import MethodCacher
 
 
-_getRecipesByInput = MethodCacher(lambda:ServerComp.CreateRecipe(ServerLevelId).GetRecipesByInput)
-_getRecipesByResult = MethodCacher(lambda:ServerComp.CreateRecipe(ServerLevelId).GetRecipesByResult)
+_getRecipesByInput = MethodCacher(
+    lambda: ServerComp.CreateRecipe(ServerLevelId).GetRecipesByInput
+)
+_getRecipesByResult = MethodCacher(
+    lambda: ServerComp.CreateRecipe(ServerLevelId).GetRecipesByResult
+)
 
 
 def GetRecipesByInput(item_id, recipe_tag, aux_value=0, maxResultNum=-1):
     # type: (str, str, int, int) -> list[dict]
     return _getRecipesByInput(item_id, recipe_tag, aux_value, maxResultNum)
 
+
 def GetRecipesByResult(item_id, recipe_tag, aux_value=0, maxResultNum=-1):
     # type: (str, str, int, int) -> list[dict]
     return _getRecipesByResult(item_id, recipe_tag, aux_value, maxResultNum)
 
+
 GetLocalTime = ServerComp.CreateDimension(ServerLevelId).GetLocalTime
 IsRaining = ServerComp.CreateWeather(ServerLevelId).IsRaining
-GetRecipeByRecipeId = MethodCacher(lambda:ServerComp.CreateRecipe(ServerLevelId).GetRecipeByRecipeId)
+GetRecipeByRecipeId = MethodCacher(
+    lambda: ServerComp.CreateRecipe(ServerLevelId).GetRecipeByRecipeId
+)
+GetSeed = MethodCacher(lambda: ServerComp.CreateGame(ServerLevelId).GetSeed)
 
 
 __all__ = [
@@ -26,5 +35,6 @@ __all__ = [
     "GetRecipesByResult",
     "GetRecipeByRecipeId",
     "GetLocalTime",
+    "GetSeed",
     "IsRaining",
 ]

e82209f15cd1689d4e79d6186bd90a81147393fa
SuperScript <2528622340@qq.com>
2026-03-16 20:19:37 +0800

    wip: 熔岩基岩钻 ui sync

--- behavior_pack/skybluetech_scripts/skybluetech/common/ui_sync/machinery/bedrock_lava_drill.py
+++ behavior_pack/skybluetech_scripts/skybluetech/common/ui_sync/machinery/bedrock_lava_drill.py
@@ -2,32 +2,24 @@
 
 from .basic_machine_ui_sync import MachineUISync
 
-K_FLUID_ID = "P"
-K_FLUID_VOLUME = "V"
-K_MAX_VOLUME = "M"
 K_RF = "r"
 K_RF_MAX = "m"
+K_LAVA_REST_PERCENT = "l"
 
 
 class BedrockLavaDrillUISync(MachineUISync):
-    fluid_id = None
-    fluid_volume = 0
-    max_volume = 0
     storage_rf = 0
     rf_max = 0
+    lava_rest_percent = 0.0
 
     def Unmarshal(self, data):
-        self.fluid_id = data[K_FLUID_ID]
-        self.fluid_volume = data[K_FLUID_VOLUME]
-        self.max_volume = data[K_MAX_VOLUME]
         self.storage_rf = data[K_RF]
         self.rf_max = data[K_RF_MAX]
+        self.lava_rest_percent = data[K_LAVA_REST_PERCENT]
 
     def Marshal(self):
         return {
-            K_FLUID_ID: self.fluid_id,
-            K_FLUID_VOLUME: self.fluid_volume,
-            K_MAX_VOLUME: self.max_volume,
             K_RF: self.storage_rf,
             K_RF_MAX: self.rf_max,
+            K_LAVA_REST_PERCENT: self.lava_rest_percent,
         }

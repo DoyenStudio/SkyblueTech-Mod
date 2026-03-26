a342f48e85d0164235b3e2ae3f25edd4b928de0b
SuperScript <2528622340@qq.com>
2026-03-26 15:10:39 +0800

    fix: 去除调试内容

--- behavior_pack/skybluetech_scripts/skybluetech/server/machinery/interfaces/energy_input_interface.py
+++ behavior_pack/skybluetech_scripts/skybluetech/server/machinery/interfaces/energy_input_interface.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from ..basic import BaseMachine, RegisterMachine
+from ..basic import RegisterMachine
 from .base_interface import BaseInterface
 
 
@@ -9,15 +9,8 @@ class EnergyInputInterface(BaseInterface):
 
     def AddPower(self, rf):
         # type: (int) -> tuple[bool, int]
-        print self.x, self.y, self.z, "GetPower", rf
         m = self.getMachineRef()
         if m is None:
-            print "But no ref"
             return False, rf
         else:
-            # import weakref
-
-            # print("Still add", weakref.getweakrefcount(m), m.x, m.y, m.z)
-            ok, res = m.AddPower(rf)
-            print self.x, self.y, self.z, "And ret", res
-            return ok, res
+            return m.AddPower(rf)

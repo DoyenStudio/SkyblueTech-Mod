6a000d6b4f6b56d8ab98107e1e3c2afe1e76cafd
SuperScript <2528622340@qq.com>
2026-03-27 10:27:24 +0800

    chore: tooldelta 侧更改

--- behavior_pack/skybluetech_scripts/skybluetech/client/user_config/key_mapping.py
+++ behavior_pack/skybluetech_scripts/skybluetech/client/user_config/key_mapping.py
@@ -5,8 +5,8 @@ MOD_NAME = "蔚蓝科技"
 
 
 RF_REPEATER_BUILD_CANCEL = C(
-    MOD_NAME, "能源中继器-取消布线", C.KEY().KEY_O, C.GAMEPAD().UNDEFINED
+    MOD_NAME, "能源中继塔-取消布线", C.KEY().KEY_O, C.GAMEPAD().UNDEFINED
 )
 RF_REPEATER_BUILD_CONFIRM = C(
-    MOD_NAME, "能源中继器-确认布线", C.KEY().KEY_F, C.GAMEPAD().UNDEFINED
+    MOD_NAME, "能源中继塔-确认布线", C.KEY().KEY_F, C.GAMEPAD().UNDEFINED
 )

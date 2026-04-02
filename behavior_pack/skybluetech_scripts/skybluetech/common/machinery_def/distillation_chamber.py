457967909d1c4202b8d6474976bffa6f435c5915
SuperScript <2528622340@qq.com>
2026-04-03 01:53:45 +0800

    chore: 小型蒸馏仓加入植物油 -> 润滑油配方

--- behavior_pack/skybluetech_scripts/skybluetech/common/machinery_def/distillation_chamber.py
+++ behavior_pack/skybluetech_scripts/skybluetech/common/machinery_def/distillation_chamber.py
@@ -1,10 +1,6 @@
 # coding=utf-8
-from ...common.define.id_enum import (
-    DISTILLED_WATER,
-    RAW_OIL,
-    LUBRICANT,
-    DISTILLATION_CHAMBER,
-)
+from ..define.id_enum import DISTILLATION_CHAMBER
+from ..define.id_enum import fluids
 from ..mini_jei.core import RecipesCollection
 from ..mini_jei.machinery.distillation_chamber import DistillatorChamberRecipe, c2k
 
@@ -12,7 +8,12 @@ from ..mini_jei.machinery.distillation_chamber import DistillatorChamberRecipe, 
 recipes = RecipesCollection(
     DISTILLATION_CHAMBER,
     DistillatorChamberRecipe(
-        "minecraft:water", 50, DISTILLED_WATER, 45, c2k(30), c2k(80), c2k(100)
+        "minecraft:water", 50, fluids.DISTILLED_WATER, 45, c2k(30), c2k(80), c2k(100)
     ),
-    DistillatorChamberRecipe(RAW_OIL, 5, LUBRICANT, 4, c2k(50), c2k(55), c2k(60)),
+    DistillatorChamberRecipe(
+        fluids.RAW_OIL, 5, fluids.LUBRICANT, 4, c2k(50), c2k(55), c2k(60)
+    ),
+    DistillatorChamberRecipe(
+        fluids.VEGETABLE_OIL, 5, fluids.LUBRICANT, 2, c2k(55), c2k(62), c2k(70)
+    ),
 )

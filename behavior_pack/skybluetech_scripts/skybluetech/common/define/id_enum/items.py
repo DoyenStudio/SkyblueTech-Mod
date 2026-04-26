5755c18f3269331e1628ac0c70dd65d387a2c00a
SuperScript <2528622340@qq.com>
2026-04-26 23:44:13 +0800

    chore: 为机器或物品配方缺失部分添加/补全物品

--- behavior_pack/skybluetech_scripts/skybluetech/common/define/id_enum/items.py
+++ behavior_pack/skybluetech_scripts/skybluetech/common/define/id_enum/items.py
@@ -126,7 +126,7 @@ class Icons:
 AIR_COMPRESS_UNIT = "skybluetech:air_compress_unit"
 CRAFTING_TEMPLATE = "skybluetech:crafting_template"
 DEACTIVATION_REDSTONE = "skybluetech:deactivation_redstone"
-DRILL_TOP = "skybluetech:drill_top"
+DRILL_TOP_STEEL = "skybluetech:drill_top_steel"
 DRILL_TOP_ULTRAHEATINUM = "skybluetech:drill_top_ultraheatinum"
 ELECTRIC_MOTOR = "skybluetech:electric_motor"
 HEAT_EXCHANGER = "skybluetech:heat_exchanger"

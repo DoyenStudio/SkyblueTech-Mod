8b19c2152d78e9e1b5b7f476eb581e2d424b4408
SuperScript <2528622340@qq.com>
2026-04-29 02:11:07 +0800

    fix: 手册页错误的机件加工台配方寻找逻辑

--- behavior_pack/skybluetech_scripts/skybluetech/client/guidance/book_custom/define/machinery_workstation_recipe_page.py
+++ behavior_pack/skybluetech_scripts/skybluetech/client/guidance/book_custom/define/machinery_workstation_recipe_page.py
@@ -1,5 +1,12 @@
 # coding=utf-8
 from skybluetech_scripts.tooldelta.ui import UBaseCtrl
+from skybluetech_scripts.tooldelta.extensions.richer_text import (
+    RicherTextCtrl,
+    RicherTextOpt,
+)
+from skybluetech_scripts.skybluetech.common.mini_jei.machinery.machinery_workstation import (
+    MachineryWorkstationRecipe,
+)
 from skybluetech_scripts.skybluetech.common.mini_jei import (
     GetRecipesByOutput,
     CategoryType,
@@ -10,10 +17,11 @@ from .base_page import BasePage
 class MachineryWorkstationRecipePage(BasePage):
     ctrl_def_name = "GuidanceLib.machinery_workstation_render_page"
 
-    def __init__(self, recipe_result):
-        # type: (str) -> None
+    def __init__(self, recipe_result, extra_text=None):
+        # type: (str, str | None) -> None
         BasePage.__init__(self)
         self.recipe_result = recipe_result
+        self.extra_text = extra_text
         self.dyn_recipe_renderer = None
 
     def RenderInit(self, ctrl):
@@ -22,17 +30,20 @@ class MachineryWorkstationRecipePage(BasePage):
 
         BasePage.RenderInit(self, ctrl)
         recipes = GetRecipesByOutput(CategoryType.ITEM, self.recipe_result)
-        if len(recipes) > 0:
-            recipe = recipes[0]
-            self.dyn_recipe_renderer = recipe.GetRendererForced()(recipe)
-            self.dyn_recipe_renderer.RenderInit(ctrl["recipe_renderer"])
-            ctrl["check_btn"].asButton().SetCallback(
-                lambda _: PushRecipeCheckerUI(recipes) and None
-            )
+        for recipe in recipes:
+            if isinstance(recipe, MachineryWorkstationRecipe):
+                self.dyn_recipe_renderer = recipe.GetRendererForced()(recipe)
+                self.dyn_recipe_renderer.RenderInit(ctrl["recipe_renderer"])
+                ctrl["check_btn"].asButton().SetCallback(
+                    lambda _: PushRecipeCheckerUI(recipes) and None
+                )
+                break
         else:
             title = ctrl["title"].asLabel()
             title.SetText("§c错误： 未找到关于 %s 的配方。" % self.recipe_result)
             title.base.SetTextFontSize(1)
+        if self.extra_text is not None:
+            RicherTextCtrl(ctrl["text_content"]).SetText(self.extra_text)
 
     def DeRender(self, ctrl):
         # type: (UBaseCtrl) -> None
